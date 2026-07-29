41cbea8bf236eac70d845cae7cd4cf85e8628103
Bering7 <riquemoura.bering@gmail.com>
2026-07-29 08:56:22 -0300

    chore: ajustando /api no URL da api

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
 // Substitua "seu-app" pelo nome real do seu web service no Render
-const API_URL = 'https://monitor-financeiro-backend.onrender.com';
+const API_URL = 'https://monitor-financeiro-backend.onrender.com/api';
 let todasTransacoes = []; // Guarda os dados na memória para usarmos na edição
 let idEdicao = null; // Controla se estamos criando (null) ou editando (número)
 
